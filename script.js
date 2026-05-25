bc57aa6cb1d804a89d9ff62f3c6fa86611b08f85
Shrishti <shrishtiagarwal205@gmail.com>
2026-05-25 14:30:47 +0530

    add changes

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-let count=0;
-let countText=document.getElementById("count");
-let increaseBtn=document.getElementById("increase");
-let decreaseBtn=document.getElementById("decrease");
-let resetBtn=document.getElementById("reset");
-increaseBtn.addEventListener("click",function()
+let count = 0;
+
+let countText = document.getElementById("count");
+
+let increaseBtn = document.getElementById("increase");
+
+let decreaseBtn = document.getElementById("decrease");
+
+let resetBtn = document.getElementById("reset");
+
+let themeBtn = document.getElementById("theme");
+
+function updateCount()
+{
+    countText.innerText = count;
+
+    if(count > 0)
+    {
+        countText.style.color = "green";
+    }
+
+    else if(count < 0)
+    {
+        countText.style.color = "red";
+    }
+
+    else
+    {
+        countText.style.color = "black";
+    }
+}
+
+increaseBtn.addEventListener("click", function()
 {
     count++;
-    countText.innerText=count;
+
+    updateCount();
 });
-decreaseBtn.addEventListener("click",function()
+
+decreaseBtn.addEventListener("click", function()
 {
-    count--;
-    countText.innerText=count;
+        count--;
+
+        updateCount();
+    
 });
-resetBtn.addEventListener("click",function()
+
+resetBtn.addEventListener("click", function()
 {
-    count=0;
-    countText.innerText=count;
+    count = 0;
+
+    updateCount();
 });
